8eda4d7a4b92aa0e253eb682ce0a1f2d1826ab93
vadsavin <vadsavin@mail.ru>
2020-03-20 16:15:19 +0300

    first commit

---
+++ 2.py
@@ -0,0 +1,8 @@
+def lol(s, *args):
+    a = []
+    for c in args:
+        a.append(s[c])
+    return a
+
+print(lol('wefvwefvwefv',1,2,3))
+    
---
+++ script.py
@@ -0,0 +1,19 @@
+from mygroup import groupmates as gm
+
+limit = float(input('Введите средний балл\n'))
+
+def print_students(students):
+    print(u"Имя".ljust(15), u"Фамилия".ljust(10),u"Экзамены".ljust(40), u"Оценки".ljust(20))
+    for student in students:
+        marks = student['marks']
+        srball = 0
+        for mark in marks:
+            srball+=int(mark)
+        srball /= len(marks)
+
+        if srball < limit:
+            continue
+        print(student["name"].ljust(15),
+        student["surname"].ljust(10), str(student["exams"]).ljust(30),str(student["marks"]).ljust(20))
+        
+print_students(gm)
